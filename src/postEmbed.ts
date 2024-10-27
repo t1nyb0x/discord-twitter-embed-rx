386aa58a893a49fcf96b8b88724ff7557319e750
t1nyb0x <dev.t1nyb0x@gmail.com>
2024-10-27 20:20:42 +0900

    メディアがないポストだと表示できない問題を修正

--- src/postEmbed.ts
+++ src/postEmbed.ts
@@ -3,7 +3,7 @@ import { VxTwitter } from "./vxtwitter/vxtwitter";
 
 export class PostEmbed {
   createEmbed(postInfo: VxTwitter): EmbedBuilder[] {
-    const embeds = postInfo.mediaURLs.map((mediaURL, index) => {
+    if (!postInfo.mediaURLs.length) {
       const embed = new EmbedBuilder()
         .setAuthor({
           name: postInfo.user_name,
@@ -14,12 +14,27 @@ export class PostEmbed {
         .setURL(postInfo.tweetURL)
         .setDescription(postInfo.text)
         .setColor(9016025)
-        .setTimestamp(new Date(postInfo.date))
-        .setImage(!/\.mp4$/.test(mediaURL) ? mediaURL : postInfo.media_extended[index].thumbnail_url);
+        .setTimestamp(new Date(postInfo.date));
 
-      return embed;
-    });
+      return [embed];
+    } else {
+      const embeds = postInfo.mediaURLs.map((mediaURL, index) => {
+        const embed = new EmbedBuilder()
+          .setAuthor({
+            name: postInfo.user_name,
+            url: "https://x.com/" + postInfo.user_screen_name,
+            iconURL: postInfo.user_profile_image_url,
+          })
+          .setTitle(postInfo.user_name)
+          .setURL(postInfo.tweetURL)
+          .setDescription(postInfo.text)
+          .setColor(9016025)
+          .setTimestamp(new Date(postInfo.date))
+          .setImage(!/\.mp4$/.test(mediaURL) ? mediaURL : postInfo.media_extended[index].thumbnail_url);
 
-    return embeds;
+        return embed;
+      });
+      return embeds;
+    }
   }
 }
