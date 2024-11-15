8db49b248d65046f87ac24ab8d2512ad496f00d3
sysnote8main <sysnote8main@gmail.com>
2024-11-15 22:30:52 +0900

    export handler and more logging

---
+++ src/discord/handler.ts
@@ -0,0 +1,27 @@
+import { Message, Client, ChannelType } from "discord.js";
+import { PostEmbed } from "../postEmbed";
+import { getTweetData } from "../shared/wrapper";
+
+const TWITTER_URL_REGEX = /https:\/\/(x|twitter)\.com\/[A-Za-z_0-9]+\/status\/[0-9]+/g;
+const postEmbed = new PostEmbed();
+
+export async function onMessageCreate(client: Client<boolean>, m: Message) {
+  if ((client.user !== null && m.author.id === client.user.id) || m.author.bot) return;
+
+  // https://twitter.com(or x.com)/hogehoge/{postID}かチェック
+  const matchRes = m.content.match(TWITTER_URL_REGEX);
+  if (matchRes) {
+    for (const i in matchRes) {
+      const tweetData = await getTweetData(matchRes[i]);
+      if (tweetData == undefined) {
+        await m.reply("ツイートの取得に失敗しました。");
+        return;
+      }
+
+      const embedPostInfo = postEmbed.createEmbed(tweetData);
+      if (m.channel.type === ChannelType.GuildText) {
+        await m.channel.send({ embeds: embedPostInfo });
+      }
+    }
+  }
+}
