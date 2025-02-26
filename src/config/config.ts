851062fac9be365dca2614eb0a2f538b5e181a40
t1nyb0x <dev.t1nyb0x@gmail.com>
2025-02-26 17:51:32 +0900

    Discordの添付サイズ制限対策：5MB以上のデータはURLを直接貼り付ける形に変更

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,22 @@
+import fs from "node:fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import yaml from "js-yaml";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// ルートディレクトリ（index.ts のディレクトリ）を固定
+// ルートディレクトリ取得
 export const ROOT_DIR = path.dirname(__dirname);
+
+const configPath = path.join(ROOT_DIR, "/config.yml");
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let config: any = {};
+try {
+  const fileContents = fs.readFileSync(configPath, "utf8");
+  config = yaml.load(fileContents);
+} catch (e) {
+  console.error("設定ファイルの読み込みに失敗しました。config.ymlを確認してください", e);
+}
+
+export default { MEDIA_MAX_FILE_SIZE: config.media_max_file_size };
---
+++ src/utils/filterMedia.ts
@@ -0,0 +1,52 @@
+import https from "https";
+import config from "../config/config";
+
+export class FilterMedia {
+  /**
+   * URLのファイルサイズを返す
+   * @param url
+   * @returns number ファイルサイズ
+   */
+  private getFileSize(url: string): Promise<number> {
+    return new Promise((resolve, reject) => {
+      const req = https.request(url, { method: "HEAD" }, (res) => {
+        const contentLength = res.headers["content-length"];
+
+        if (contentLength) {
+          resolve(parseInt(contentLength, 10));
+        } else {
+          reject(new Error("Could not get Content-Length Header... "));
+        }
+      });
+
+      req.on("error", reject);
+      req.end();
+    });
+  }
+
+  /**
+   * MEDIA_MAX_FILE_SIZEで指定したサイズより小さいものと大きいもので分ける
+   * @param mediaUrls
+   * @returns {validUrls: string[]; invalidUrls: string[]} validUrls - MEDIA_MAX_FILE_SIZEより小さいデータURL配列 invalidUrls - MEDIA_MAX_FILE_SIZEより大きなデータURL配列
+   */
+  async mediaSizeFilter(mediaUrls: string[]): Promise<{ validUrls: string[]; invalidUrls: string[] }> {
+    const validUrls: string[] = [];
+    const invalidUrls: string[] = [];
+
+    await Promise.all(
+      mediaUrls.map(async (url) => {
+        try {
+          const fileSize = await this.getFileSize(url);
+          if (fileSize <= config.MEDIA_MAX_FILE_SIZE) {
+            validUrls.push(url);
+          } else {
+            invalidUrls.push(url);
+          }
+        } catch (error) {
+          console.error(new Error(`Error checking file size ${error}`));
+        }
+      })
+    );
+    return { validUrls: validUrls, invalidUrls: invalidUrls };
+  }
+}
