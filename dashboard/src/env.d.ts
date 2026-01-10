65f6439679a65cafd69797f0b8c42019a3c25a84
t1nyb0x <dev.t1nyb0x@gmail.com>
2026-01-10 19:16:42 +0900

    chore: remove dashboard directory (moving to separate repo)

--- dashboard/src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="astro/client" />
-
-declare namespace App {
-  interface Locals {
-    user: import("lucia").User | null;
-    session: import("lucia").Session | null;
-    url: URL;
-  }
-}
